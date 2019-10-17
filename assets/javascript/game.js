f64197013e74c2aaedb598d41551e6fc4b60e010
Tommy Flynn <TFly.Flynn@gmail.com>
2019-10-17 09:41:29 -0400

    made changes to userGuess and got losses increasing by one when the guessesLeft = <1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,9 @@ var guessesLeft = 9;
 //   There is definitely a better way to set up this array, but in the interest of not adding more headache, I'm just going to write it all out. 
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+// Need a variable to push the user submission to an array. 
+var lettersGuessed = []
+
 //Need to create variables that hold references to the places in the HTML where we want to display things. 
 
 var winsText = document.getElementById("wins-text");
@@ -17,14 +20,17 @@ var lossesText = document.getElementById("losses-text");
 var guessesLeftText = document.getElementById("guessesLeft-text");
 var userChoiceText = document.getElementById("guessesSoFar-text");
 
+// This is to have the display amount of guesses left to 9: 
+guessesLeftText.textContent = guessesLeft;
+winsText.textContent = wins;
+
+  // Now I need to randomly choose a choice from the 26 letter options array. Still in the same function of user pressing a key. But how do I get the random choice to survive 
+  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 //This function is run whenever the user presses a key
 document.onkeyup = function (event) {
     // This is defining the object userText as the result of the above function + key
     var userGuess = event.key;
-
-    // Now I need to randomly choose a choice from the 26 letter options array. Still in the same function of user pressing a key. But how do I get the random choice to survive 
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
     //going to console.log computerGuess and userGuess to make sure they are recording (before I assign them to spans).
     console.log(computerGuess);
@@ -37,13 +43,18 @@ document.onkeyup = function (event) {
     //Let's start with the easy one. If the guesses are the same, wins increases by one. 
     if (userGuess === computerGuess) {
         wins++;
+        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        guessesLeft =9; 
     }
     // Now I need to account for all other variables. Luckily, anything that doesn't equal computerGuess results in an incorrect guess. So I need two outcomes: 1. GuessesLeft decreases by 1, and 2. When GuessesLeft =0, then losses = +1. 
     else {
         guessesLeft--;
+        lettersGuessed.push(userGuess);
         // HOW DO I GET THE BELOW FUNCTION TO WORK?
-        if (guessesLeftText < 1){
-            losses++;
+        if (guessesLeft < 1){
+            losses++; 
+            guessesLeft =9; 
+            computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
         }
     }
     //Displays the wins applying:
@@ -54,9 +65,14 @@ document.onkeyup = function (event) {
     // So far this accounts for wins increasing, and guesses decreasing (although it doesn't display 9 yet)
 
     // Now I need to have make a way for losses to increase by 1 if guesses left reaches 0. 
+    lossesText.textContent= losses;
 
     // I also need to display the user's guess. I'll do that using userText. 
-    userChoiceText.textContent = userGuess;
+    userChoiceText.textContent = lettersGuessed.join(",");
+
+    // Need to display 
+    
+   
     // BUT I need these to list up until 9 guesses, rather than resubmitting a number each time. 
     
 }
