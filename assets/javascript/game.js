f6c7b5255573d3c56d7c2dad552c1f029565d2b1
Tommy Flynn <TFly.Flynn@gmail.com>
2019-10-16 22:19:35 -0400

    added user input to read to screen. Still need the following changes: 1. screen needs to display 9 to start. 2. computer choice needs to remain the same until user guesses correctly, not a new number each time. 3. When guesses reaches 0, losses needs to increase to. Also, line 45 of my code isn't currently accomplishing anything.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,11 @@
 // Going to define some variables to start at 0. 
+
+// Number of wins to start:
 var wins = 0;
+// number of losses to start:
 var losses = 0;
+// number of guesses left to start:
+var guessesLeft = 9;
 //   There is definitely a better way to set up this array, but in the interest of not adding more headache, I'm just going to write it all out. 
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -10,33 +15,48 @@ var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 // This particular var isn't currently doing anything. I need to define "guessesLeftText"
 var guessesLeftText = document.getElementById("guessesLeft-text");
-var userText = document.getElementById("guessesSoFar-text");
+var userChoiceText = document.getElementById("guessesSoFar-text");
 
 
 //This function is run whenever the user presses a key
 document.onkeyup = function (event) {
     // This is defining the object userText as the result of the above function + key
-   var userGuess = event.key;
+    var userGuess = event.key;
 
-    // Now I need to randomly choose a choice from the 26 letter options array. Still in the same function of user pressing a key. 
-var computerGuess = computerChoices [Math.floor(Math.random() * computerChoices.length)];
+    // Now I need to randomly choose a choice from the 26 letter options array. Still in the same function of user pressing a key. But how do I get the random choice to survive 
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-//going to console.log computerGuess and userGuess to make sure they are recording (before I assign them to spans).
-console.log (computerGuess);
-console.log (userGuess);
+    //going to console.log computerGuess and userGuess to make sure they are recording (before I assign them to spans).
+    console.log(computerGuess);
+    console.log(userGuess);
 
-//both working properly. 
+    //both working properly. 
 
-//Now I need to apply the if/then logic to determine the outcome of the game. 
+    //Now I need to apply the if/then logic to determine the outcome of the game. 
 
-//Let's start with the easy one. If the guesses are the same, wins increases by one. 
-if (userGuess === computerGuess) 
-{
-    wins++;
+    //Let's start with the easy one. If the guesses are the same, wins increases by one. 
+    if (userGuess === computerGuess) {
+        wins++;
+    }
+    // Now I need to account for all other variables. Luckily, anything that doesn't equal computerGuess results in an incorrect guess. So I need two outcomes: 1. GuessesLeft decreases by 1, and 2. When GuessesLeft =0, then losses = +1. 
+    else {
+        guessesLeft--;
+        // HOW DO I GET THE BELOW FUNCTION TO WORK?
+        if (guessesLeftText < 1){
+            losses++;
+        }
+    }
+    //Displays the wins applying:
+    winsText.textContent = wins;
+    // Displays the guesses applying:
+    guessesLeftText.textContent = guessesLeft;
+
+    // So far this accounts for wins increasing, and guesses decreasing (although it doesn't display 9 yet)
+
+    // Now I need to have make a way for losses to increase by 1 if guesses left reaches 0. 
+
+    // I also need to display the user's guess. I'll do that using userText. 
+    userChoiceText.textContent = userGuess;
+    // BUT I need these to list up until 9 guesses, rather than resubmitting a number each time. 
+    
 }
-
-winsText.textContent = wins;
-}
-// Displaying the wins applying: 
-
-// Displaying the wins applying: 
